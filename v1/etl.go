17948dc04332978476ac0ed5bcd8b82bf9719026
sujit <s.baniya.np@gmail.com>
2025-03-08 04:34:41 +0545

    feat: custom query

--- v1/etl.go
+++ v1/etl.go
@@ -2,17 +2,11 @@ package v1
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
 	"log"
 	"sync"
 
-	"github.com/oarkflow/sql/v1/config"
 	"github.com/oarkflow/sql/v1/contracts"
-	"github.com/oarkflow/sql/v1/loaders"
-	"github.com/oarkflow/sql/v1/mappers"
-	"github.com/oarkflow/sql/v1/sources"
-	"github.com/oarkflow/sql/v1/transformers"
 )
 
 // ETLJob encapsulates the ETL pipeline components.
@@ -24,126 +18,6 @@ type ETLJob struct {
 	workerCount   int
 	batchSize     int
 	rawChanBuffer int
-}
-
-// Option is a function that configures an ETLJob.
-type Option func(*ETLJob) error
-
-// WithSource configures the source based on the provided parameters.
-func WithSource(sourceType string, sourceDB *sql.DB, sourceFile, sourceTable, sourceQuery string) Option {
-	return func(e *ETLJob) error {
-		var src contracts.Source
-		switch sourceType {
-		case "mysql", "postgresql":
-			if sourceDB == nil {
-				return fmt.Errorf("source database is nil")
-			}
-			src = sources.NewSQLSource(sourceDB, sourceTable, sourceQuery)
-		case "csv":
-			file := sourceTable
-			if file == "" {
-				file = sourceFile
-			}
-			src = sources.NewCSVSource(file)
-		case "json":
-			file := sourceTable
-			if file == "" {
-				file = sourceFile
-			}
-			src = sources.NewJSONSource(file)
-		default:
-			return fmt.Errorf("unsupported source type: %s", sourceType)
-		}
-		e.source = src
-		return nil
-	}
-}
-
-// WithDestination configures the loader based on the provided parameters.
-func WithDestination(destType string, destDB *sql.DB, destFile string, cfg config.TableMapping) Option {
-	return func(e *ETLJob) error {
-		var loader contracts.Loader
-		switch destType {
-		case "mysql", "postgresql":
-			if destDB == nil {
-				return fmt.Errorf("destination database is nil")
-			}
-			if cfg.KeyValueTable {
-				loader = loaders.NewKeyValueLoader(destDB, destType, cfg)
-			} else {
-				loader = loaders.NewSQLLoader(destDB, destType, cfg)
-			}
-		case "csv":
-			file := cfg.NewName
-			if file == "" {
-				file = destFile
-			}
-			loader = loaders.NewCSVLoader(file)
-		case "json":
-			file := cfg.NewName
-			if file == "" {
-				file = destFile
-			}
-			loader = loaders.NewJSONLoader(file)
-		default:
-			return fmt.Errorf("unsupported destination type: %s", destType)
-		}
-		e.loader = loader
-		return nil
-	}
-}
-
-// WithMapping configures the field mappers.
-func WithMapping(mapping map[string]string) Option {
-	return func(e *ETLJob) error {
-		mappersList := []contracts.Mapper{}
-		if len(mapping) > 0 {
-			mappersList = append(mappersList, mappers.NewFieldMapper(mapping))
-		}
-		// Always add a default mapper to convert field names to lowercase.
-		mappersList = append(mappersList, &mappers.LowercaseMapper{})
-		e.mappers = mappersList
-		return nil
-	}
-}
-
-// WithTransformers configures the transformers.
-func WithTransformers() Option {
-	return func(e *ETLJob) error {
-		transformersList := []contracts.Transformer{
-			&transformers.LookupTransformer{
-				LookupData:  map[string]string{"key1": "value1"},
-				Field:       "some_field",
-				TargetField: "lookup_result",
-			},
-		}
-		e.transformers = transformersList
-		return nil
-	}
-}
-
-// WithWorkerCount sets the number of concurrent workers.
-func WithWorkerCount(count int) Option {
-	return func(e *ETLJob) error {
-		e.workerCount = count
-		return nil
-	}
-}
-
-// WithBatchSize sets the batch size for loading.
-func WithBatchSize(size int) Option {
-	return func(e *ETLJob) error {
-		e.batchSize = size
-		return nil
-	}
-}
-
-// WithRawChanBuffer sets the buffer size for the raw channel.
-func WithRawChanBuffer(buffer int) Option {
-	return func(e *ETLJob) error {
-		e.rawChanBuffer = buffer
-		return nil
-	}
 }
 
 // NewETLJob creates an ETLJob with default settings and applies all provided options.
