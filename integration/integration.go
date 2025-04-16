54546fef1afccbd2d6c7506536c33d57d9ba015e
sujit <s.baniya.np@gmail.com>
2025-04-16 09:58:44 +0545

    u

--- integration/integration.go
+++ integration/integration.go
@@ -902,7 +902,7 @@ func main() {
 	defer cancel()
 
 	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, os.Interrupt, os.Kill)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		sig := <-sigCh
 		log.Printf("Received signal: %v, initiating shutdown", sig)
